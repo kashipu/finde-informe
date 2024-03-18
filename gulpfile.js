e2f41e1a6dc520edf34b6d67de401245805da61d
William Moreno <william.moreno@periferia-it.com>
2024-03-17 20:18:33 -0500

    Primera maqueta

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+import gulp from 'gulp';
+import pug from 'gulp-pug';
+import * as nodesass from 'sass';
+import gulpsass from 'gulp-sass';
+import imagemin from 'gulp-imagemin'
+const sass = gulpsass(nodesass)
+
+
+gulp.task('views', () => {
+  return gulp.src('./src/views/*.pug')
+  .pipe(pug({ pretty: true }))
+  .pipe(gulp.dest('./public'))
+})
+
+gulp.task('styles', () => {
+  return gulp.src('./src/scss/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./public/css'))
+  //{outputStyle: 'compressed'}
+})
+
+gulp.task('imageop', () => {
+  return gulp.src('./src/images/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./public/images'))
+})
+
+gulp.task('default', () => {
+  gulp.watch('./src/views/**/*.pug', gulp.series('views'))
+  gulp.watch('./src/scss/**/*.scss', gulp.series('styles'))
+  gulp.watch('./src/images/**/*', gulp.series('imageop'))
+})
