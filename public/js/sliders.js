ff16e0a5e178bad86babb814e8c1313099e06793
William Moreno <william.moreno@periferia-it.com>
2024-04-10 21:04:26 -0500

    Finanzas responsables

--- public/js/sliders.js
+++ public/js/sliders.js
@@ -1,23 +1,29 @@
-function mostrarCifras(selectorCards, selectorCifras) {
+function mostrarCifras(selectorCards, selectorCifras, activeClass) {
   const cards = document.querySelectorAll(selectorCards);
   const cifras = document.querySelectorAll(selectorCifras);
-
+  console.log(cards)
+  console.log(cifras)
+  cards[0].classList.add(activeClass)
   cards.forEach((card, index) => {
     card.addEventListener('click', function() {
       cifras.forEach(cifra => cifra.style.display = 'none');
       cifras[index].style.display = 'block';
+      console.log(cifras)
     });
+    card.addEventListener("click", function(){
+      cards.forEach(card => card.classList.remove(activeClass))
+      cards[index].classList.add(activeClass)
+
+    })
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  mostrarCifras('.card-uno', '.cifras-item');
-  mostrarCifras('.card-dos', '.cifra-space-box');
-  mostrarCifras('.card-uno', '.texto-box');
-  mostrarCifras('.ambiental-button', '.slide-content');
-  mostrarCifras('.card-uno', '.finanzas-slider-item');
-  mostrarCifras('.slider-asistencia-item', '.asistencia-textimage-box');
+/* document.addEventListener('DOMContentLoaded', function() {
+  mostrarCifras('.card-uno', '.cifras-item', "card-uno-active");
+  mostrarCifras('.card-dos', '.cifra-space-box', "active");
+  mostrarCifras('.card-uno', '.texto-box', "active");
+  mostrarCifras('.ambiental-button', '.slide-content', "active");
+  mostrarCifras('.card-uno', '.finanzas-slider-item', "active");
+  mostrarCifras('.slider-asistencia-item', '.asistencia-textimage-box', "active");
 });
-
-console.log(document.querySelectorAll(".slider-asistencia-item"))
-console.log(document.querySelectorAll(".finanzas-slider-item"))
+ */
