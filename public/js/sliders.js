932e83faf7cadc826fc71bf3ee8e97f68777c525
William Moreno <william.moreno@periferia-it.com>
2024-04-19 14:39:32 -0500

    Ajuste sliders scroll

--- public/js/sliders.js
+++ public/js/sliders.js
@@ -1,19 +1,24 @@
 function mostrarCifras(selectorCards, selectorCifras, activeClass) {
   const cards = document.querySelectorAll(selectorCards);
   const cifras = document.querySelectorAll(selectorCifras);
-  console.log(cards)
-  console.log(cifras)
+  const scrollAmount = 400;
+  let scrollDone = false;
   cards[0].classList.add(activeClass)
   cards.forEach((card, index) => {
     card.addEventListener('click', function() {
       cifras.forEach(cifra => cifra.style.display = 'none');
       cifras[index].style.display = 'block';
-      console.log(cifras)
+      if (!scrollDone) {
+        window.scrollBy({
+          top: scrollAmount,
+          behavior: 'smooth'
+        });
+        scrollDone = true;
+      }
     });
     card.addEventListener("click", function(){
       cards.forEach(card => card.classList.remove(activeClass))
       cards[index].classList.add(activeClass)
-
     })
   });
 }
